63c131f92cd2924e71451c65cf9feaf04c21f4e6
Ty Todd <swagginty@gmail.com>
2025-02-16 17:14:48 -0500

    removed more of brukes peasantry

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from app import get_app, get_socket
 import api
 import threading
 import time
-from serial_comms import driver
 
 app = get_app()
 socketio = get_socket()
@@ -13,7 +12,7 @@ ui_connected_event = threading.Event()
 def flask():
     socketio.run(app, port=8000, debug=True, use_reloader=False)
 
-def man():
+def main():
     while True:
         for i in range(3):
             time.sleep(1)
@@ -21,5 +20,5 @@ def man():
         api.item_pickup(0)
 
 if __name__ == "__main__":
-    threading.Thread(target=driver).start()  # Run main() in a different thread
+    threading.Thread(target=main).start()  # Run main() in a different thread
     flask()  # Run flask() in the main thread
