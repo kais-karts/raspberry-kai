9aff483501912b8f59c4b245d14fcdc8d3f9e7ba
Ty Todd <swagginty@gmail.com>
2025-02-16 00:55:48 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,90 @@
+import numpy as np
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+class Kart:
+    def __init__(self, id, name, current_item, location):
+        self.id = id
+        self.name = name
+        self.current_item = current_item
+        self.location = location
+
+    def get_name(self):
+        # Remove the dash and capitalize the name
+        return self.name.replace("-", " ").title()
+
+    def __str__(self):
+        return f"{self.name, self.location, self.current_item}"
+
+
+def scale_location(location, map_width, map_height):
+    x, y = location
+    return (x / map_width * 100, y / map_height * 100)
+
+
+# Register the custom filter
+@app.template_filter("scale_location")
+def scale_location_filter(location):
+    map_width = 300
+    map_height = 400
+    return scale_location(location, map_width, map_height)
+
+
+karts = []
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    karts = [
+        Kart(1, "mario", "Banana", (150, 150)),
+        Kart(2, "luigi", "Green Shell", (200, 115)),
+        Kart(3, "Toad", "Mushroom", (32, 240)),
+        Kart(4, "bowser", "Fireball", (91, 67)),
+        Kart(5, "donkey-kong", "Barrel", (190, 230)),
+        Kart(6, "waluigi", "Bob-omb", (150, 320)),
+    ]
+    return render_template("index.html", karts=karts)
+
+
+def get_ranking():
+    """Rerank the karts based on their position"""
+    pass
+
+
+def use_item(kart: Kart):
+    """Upon request from a kart, use their active item"""
+
+    kart.current_item = None
+
+
+def get_item():
+    """
+    Get a random item from the item list
+    Triggered when kart passes a checkpoint
+    """
+    pass
+
+
+def get_message():
+    """
+    Called when a kart sends a message to the server
+    """
+    message = ""
+    id = 0
+    new_position = (0, 0)
+    if message == "update":
+        update_position(id, new_position)
+    elif message == "ability":
+        use_item()
+
+
+def update_position(id, new_position):
+    # Get kart by id from karts list
+    kart = [kart for kart in karts if kart.id == id][0]
+    kart.location = new_position
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
