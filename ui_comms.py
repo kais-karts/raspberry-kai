600273616c00752116b1d3db96b71ce1d3d32dfa
Bruke Wossenseged <brukew@mit.edu>
2025-02-19 18:21:44 -0500

    send corrrect info to item hit

--- ui_comms.py
+++ ui_comms.py
@@ -59,6 +59,7 @@ async def item_hit(item: int, duration: int) -> None:
 
     msg = json.dumps({
         "action": "item_hit",
+        "item": item
         "duration": duration,
     })
     await websocket_conn.send(msg)
